574f3cb1640f3e9fb3fa94d13f9f206f7e6b6233
kudlav <vladankudlac@gmail.com>
2018-04-18 15:05:02 +0200

    FileManager: fixed getVideoFiles, add getVideoAttachments
    Bugfix: getVideoFiles unknown columns from file table.
    Add getVideoAttachments.
    Add comments and parameters types.

--- app/model/FileManager.php
+++ app/model/FileManager.php
@@ -3,6 +3,7 @@
 namespace App\Model;
 
 use Nette;
+use Nette\Database\Context;
 
 
 class FileManager
@@ -22,45 +23,62 @@ class FileManager
 	;
 
 
-	/** @var Nette\Database\Context */
+	/**
+	 * @var array $parameters
+	 * @var Context $database
+	 */
 	private $parameters, $database;
 
-	public function __construct($parameters, Nette\Database\Context $database)
+	public function __construct(array $parameters, Context $database)
 	{
 		$this->parameters = $parameters;
 		$this->database = $database;
 	}
 
 	/**
+	 * Get array of video files.
 	 *
-	 *
-	 * @param $videoId
-	 * @return array
+	 * @param int $videoId Video ID.
+	 * @return array Associative array [videoType => videoPath].
 	 */
-	public function getVideoFiles($videoId)
+	public function getVideoFiles(int $videoId)
 	{
 		$type = ['video/mp4', 'video/webm', 'video/ogg'];
-		return $this->getVideoFile($videoId, $type)->fetchPairs(self::FILE_TYPE, self::FILE_PATH);
+		return $this->getVideoFile($videoId, $type)
+			->select('file.type AS type, file.path AS path')
+			->fetchPairs(self::FILE_TYPE, self::FILE_PATH);
 	}
 
 	/**
+	 * Get row of video thumbnail.
 	 *
-	 *
-	 * @param $videoId
-	 * @return false|Nette\Database\Table\ActiveRow
+	 * @param int $videoId Video ID.
+	 * @return FALSE|Nette\Database\Table\ActiveRow ActiveRow if thumbnail exists, otherwise return FALSE.
 	 */
-	public function getVideoThumbnail($videoId)
+	public function getVideoThumbnail(int $videoId)
 	{
 		$type = 'thumbnail';
 		return $this->getVideoFile($videoId, $type)->fetch();
 	}
 
 	/**
+	 * Get rows with video attachments.
 	 *
+	 * @param int $videoId Video ID.
+	 * @return Nette\Database\Table\Selection Selection of video attachment rows.
+	 */
+	public function getVideoAttachments(int $videoId)
+	{
+		$type = 'attachment';
+		return $this->getVideoFile($videoId, $type);
+	}
+
+	/**
+	 * Get rows with files of specified type connected with video.
 	 *
-	 * @param $videoId
-	 * @param $type
-	 * @return Nette\Database\Table\Selection
+	 * @param int $videoId Video ID.
+	 * @param string $type Type of file.
+	 * @return Nette\Database\Table\Selection Selection of certain type files connected to video.
 	 */
 	private function getVideoFile($videoId, $type)
 	{
